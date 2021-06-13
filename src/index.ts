5e8b56b3d595d8c47d983e7f395d2e1ff0d372cb
nazmuul420 <nazmuul420@gmail.com>
2021-06-13 21:27:40 +0600

    Login and Authorization -> @Authorized and @UseMiddleware decorator!

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,39 @@
 import "reflect-metadata";
 import { ApolloServer, ApolloError } from "apollo-server-express";
 import Express from "express";
+import session from "express-session";
+import connectRedis from "connect-redis";
+import cors from "cors";
 import { GraphQLError, GraphQLFormattedError } from "graphql";
 import { buildSchema, ArgumentValidationError } from "type-graphql";
 import { createConnection } from "typeorm";
 
 import { RegisterResolver } from "./modules/user/Register";
+import { LoginResolver } from "./modules/user/Login";
+import { redis } from "./redis";
+import { MeResolver } from "./modules/user/Me";
+
+// Declaration Merging to add types in express req.session
+declare module "express-session" {
+  interface SessionData {
+    userId: number;
+  }
+}
 
 const main = async () => {
   await createConnection();
 
   const schema = await buildSchema({
-    resolvers: [RegisterResolver],
+    resolvers: [MeResolver, RegisterResolver, LoginResolver],
+    // https://typegraphql.com/docs/authorization.html
+    authChecker: ({ context: { req } }) => {
+      // for @Authorized() decorator
+      if (req.session.userId) {
+        return true;
+      }
+
+      return false; // access is denied
+    },
   });
 
   const apolloServer = new ApolloServer({
@@ -40,9 +62,36 @@ const main = async () => {
 
       return error;
     },
+    context: ({ req }: any) => ({ req }),
   });
 
   const app = Express();
+
+  const RedisStore = connectRedis(session);
+
+  app.use(
+    cors({
+      credentials: true,
+      origin: true,
+    })
+  );
+
+  app.use(
+    session({
+      store: new RedisStore({
+        client: redis,
+      }),
+      name: "qid",
+      secret: "qwertyuiop",
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+      },
+    })
+  );
 
   apolloServer.applyMiddleware({ app });
 
